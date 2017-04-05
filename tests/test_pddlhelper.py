2ce9701b7ad021aa730973c2b1f2b94333a80c50
Ivo <ivaylokhr@gmail.com>
2017-04-05 20:04:27 +0100

    ctrl alt l

--- tests/test_pddlhelper.py
+++ tests/test_pddlhelper.py
@@ -4,20 +4,18 @@ import os.path
 
 
 class PddlHelperTestCase(unittest.TestCase):
-
     def test_badfile(self):
         _filepath = os.path.join(os.path.dirname(__file__), 'sample_pddl.txt')
         new_timing = pddlhelper._get_new_stages_information(_filepath)
         self.assertEqual(
             new_timing, {})
 
-
     def test_goodfile(self):
         _filepath = os.path.join(os.path.dirname(__file__), 'plan_example.pddl')
         new_timing = pddlhelper._get_new_stages_information(_filepath)
         self.assertEqual(
-            new_timing, {'junction_G' : ['35','40','45','80'],
-                                      'junction_E' : ['35','45','80']})
+            new_timing, {'junction_G': ['35', '40', '45', '80'],
+                         'junction_E': ['35', '45', '80']})
 
 
 if __name__ == '__main__':
