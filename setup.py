cfbc41b27c205025ee0bc1b2e485c514fafe888d
Ivo <ivaylokhr@gmail.com>
2017-05-01 22:50:47 +0100

    New setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ivaylotfl',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=['pywin32'],
     entry_points={
